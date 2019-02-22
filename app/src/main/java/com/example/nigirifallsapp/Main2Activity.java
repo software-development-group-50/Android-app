ca27dfc3c3706c3189a5a76c2447cd530e6a6110
theodor <theodolh@stud.ntnu.no>
2019-02-22 13:45:26 +0100

    state_functionality

---
+++ app/src/main/java/com/example/nigirifallsapp/Main2Activity.java
@@ -0,0 +1,36 @@
+package com.example.nigirifallsapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class Main2Activity extends AppCompatActivity {
+
+    TextView showValue;
+    int counter = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        showValue = (TextView) findViewById(R.id.countervalue);
+    }
+
+
+    //ved hvert klikk på denne knappen vil verdien til countervalue øke
+    public void countIN(View view){
+        counter++;
+        showValue.setText(Integer.toString(counter));
+    }
+
+    //ved hvert klikk på denne knappen vil verdien til countervalue minke
+    public void countDown(View v){
+        counter--;
+        showValue.setText(Integer.toString(counter));
+    }
+
+
+
+}
--- app/src/main/java/com/example/nigirifallsapp/MainActivity.java
+++ app/src/main/java/com/example/nigirifallsapp/MainActivity.java
@@ -2,6 +2,13 @@ package com.example.nigirifallsapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.content.Intent;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Button;
+import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +16,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Button button = (Button) findViewById(R.id.button);
+
+        goToSecondActivity();
+
     }
+
+
+    private void goToSecondActivity() {
+        Button button = (Button) findViewById(R.id.button2);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, SecondActivity.class));
+
+                //goToSecondActivity(new Intent(MainActivity.this, SecondActivity.class));
+            }
+        });
+    }
+/*
+    public void goToThirdActivity(View view){
+        Intent intent = new Intent(this, SecondActivity.class);
+
+        startActivity(intent);
+    }
+*/
+
+
+
 }
