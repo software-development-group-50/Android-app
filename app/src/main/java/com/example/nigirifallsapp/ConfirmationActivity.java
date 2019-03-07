fcc9851b94e01f1ee3062984fbf3824d7ee3e950
Petter Sletsjøe <petter.sletsjoe@gmail.com>
2019-03-07 15:57:56 +0100

    retrieving OrderID from database

--- app/src/main/java/com/example/nigirifallsapp/ConfirmationActivity.java
+++ app/src/main/java/com/example/nigirifallsapp/ConfirmationActivity.java
@@ -1,13 +1,33 @@
 package com.example.nigirifallsapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ConfirmationActivity extends AppCompatActivity {
+    private String orderID = "nothing";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.updateOrderID();
         setContentView(R.layout.confirmation_activity_layout);
+
+        TextView textView2 = findViewById(R.id.textView2);
+        String text = "Your order has been registered! Ready for pickup in about 30 min. \n\nPickup reference: ";
+        text += getOrderID();
+        textView2.setText(text); //append(this.orderID);
+    }
+
+    private void updateOrderID(){
+        String newOrderID = getIntent().getStringExtra("OrderID");
+
+        if (newOrderID != null) {
+            this.orderID = newOrderID;
+        }
+    }
+    private String getOrderID(){
+        return this.orderID;
     }
 }
