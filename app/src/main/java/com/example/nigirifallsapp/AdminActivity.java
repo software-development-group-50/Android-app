24d84d9c67ce5d79e38c0b957957ba4c78ed900c
Magnus Tidemann <magnuti@stud.ntnu.no>
2019-03-08 14:48:31 +0100

    Now you can fetch all orders. Layout fix. Toggle between orders. Added buttons. 10/10

--- app/src/main/java/com/example/nigirifallsapp/AdminActivity.java
+++ app/src/main/java/com/example/nigirifallsapp/AdminActivity.java
@@ -1,13 +1,121 @@
 package com.example.nigirifallsapp;
 
+import android.content.Context;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Layout;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class AdminActivity extends AppCompatActivity {
+
+    RequestQueue requestQueue;
+    Button buttonConfirm;
+    Button buttonFinish;
+    LinearLayout linearLayoutAdmin;
+    private int chosenDishID;
+    private int defaultColor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin);
+        this.requestQueue = Volley.newRequestQueue(this);
+        this.buttonConfirm = findViewById(R.id.buttonConfirm);
+        this.buttonFinish = findViewById(R.id.buttonFinish);
+        this.linearLayoutAdmin = findViewById(R.id.linearLayoutAdmin);
+        this.sendRequest("http://folk.ntnu.no/magnuti/getallorders.php");
+    }
+
+    // Function for sending a HTTP request to the PHP-script
+    private void sendRequest(String url) {
+        // The requests are sent in cleartext over HTTP. Use HTTPS when sending passwords.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                onActualResponse(response); // The extra function is needed because of the scope of the @Override function
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+
+        requestQueue.add(stringRequest);
+    }
+
+    private void onActualResponse(String response){
+        List<OrderInAdmin> orderList = new ArrayList<>();
+        String[] arrayWithStringOrders = response.split(";");
+        for (String elementsInStringArray : arrayWithStringOrders) {
+            OrderInAdmin orderInAdmin = new OrderInAdmin(elementsInStringArray);
+            orderList.add(orderInAdmin);
+        }
+        addMenuToView(orderList);
+    }
+
+    // Function for addin..
+    private void addMenuToView(List<OrderInAdmin> orderInAdminList){ //Parameter Menu-class
+        for (int i = 0; i < orderInAdminList.size(); i++){
+            LayoutInflater outerLayoutInflater = (LayoutInflater) getApplicationContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            View orderInAdminView = outerLayoutInflater.inflate(R.layout.order_in_admin_layout, null);
+
+            final TextView textOrders = orderInAdminView.findViewById(R.id.textOrderID);
+            final TextView textPickUpTime = orderInAdminView.findViewById(R.id.textPickUpTime);
+            final TextView textOrderStatus = orderInAdminView.findViewById(R.id.textOrderStatus);
+
+            textOrders.setText(orderInAdminList.get(i).getOrderId());
+            textPickUpTime.setText(orderInAdminList.get(i).getPickUpTime());
+            textOrderStatus.setText(orderInAdminList.get(i).getStatus());
+
+            ViewGroup outerInsertPoint = (ViewGroup) findViewById(R.id.linearLayoutAdmin);
+            outerInsertPoint.addView(orderInAdminView, -1, new ViewGroup.LayoutParams(ViewGroup.LayoutParams.FILL_PARENT, ViewGroup.LayoutParams.FILL_PARENT));
+            // The -1 index specifies that the element is added LAST to the ScrollView
+
+            for (int k = 0; k < orderInAdminList.get(i).getDishList().size(); k += 2){
+                LayoutInflater innerLayoutInflater = (LayoutInflater) getApplicationContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                View dishInOrderInAdminView = innerLayoutInflater.inflate(R.layout.dish_in_order_in_admin_layout, null);
+
+                TextView textDishName = dishInOrderInAdminView.findViewById(R.id.textDishName);
+                TextView textQuantity = dishInOrderInAdminView.findViewById(R.id.textQuantity);
+
+                textDishName.setText(orderInAdminList.get(i).getDishList().get(k));
+                textQuantity.setText(orderInAdminList.get(i).getDishList().get(k + 1));
+                ViewGroup innerInsertPoint = (ViewGroup) findViewById(R.id.linearLayoutOrder);
+                innerInsertPoint.addView(dishInOrderInAdminView, -1, new ViewGroup.LayoutParams(ViewGroup.LayoutParams.FILL_PARENT, ViewGroup.LayoutParams.FILL_PARENT));
+            }
+
+            final LinearLayout linearLayoutOrder = findViewById(R.id.linearLayoutOrder);
+            this.defaultColor = linearLayoutOrder.getSolidColor(); // I don´t know the default color in Android Studio, so it is fetched here.
+            linearLayoutOrder.setId(i); // This line is required so that not all orders are placed into the same linearLayoutOrder.
+            final int linearLayoutOrderId = linearLayoutOrder.getId();
+
+            this.linearLayoutAdmin.getChildAt(linearLayoutOrderId).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    linearLayoutAdmin.getChildAt(chosenDishID).setBackgroundColor(defaultColor);
+                    linearLayoutAdmin.getChildAt(linearLayoutOrderId).setBackgroundColor(Color.GRAY);
+                    chosenDishID = linearLayoutOrderId;
+                }
+            });
+        }
     }
 }
