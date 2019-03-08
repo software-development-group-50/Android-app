d7d06874a90b4b5bb316c55d7cae6e28193dda39
theodor <theodolh@stud.ntnu.no>
2019-03-08 11:38:31 +0100

    login and registration functionality

--- app/src/main/java/com/example/nigirifallsapp/LoginActivity.java
+++ app/src/main/java/com/example/nigirifallsapp/LoginActivity.java
@@ -1,13 +1,84 @@
 package com.example.nigirifallsapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
 
 public class LoginActivity extends AppCompatActivity {
+
+    Button loginBtn;
+    Button registerBtn;
+    EditText phoneNr;
+    EditText password;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        phoneNr = (EditText)findViewById(R.id.input_nr);
+        password = (EditText)findViewById(R.id.input_password);
+        this.loginBtn = findViewById(R.id.login_login);
+        this.registerBtn = findViewById(R.id.Register1);
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                validate(phoneNr.getText().toString(), password.getText().toString());
+            }
+        });
+
+        registerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
     }
+
+    /*
+    //Function for moving to the Menu-activity
+    public void onButtonMenu(View view){
+
+
+    }
+
+
+    //move to registerActivity
+    public void onButtonRegister(View view){
+
+
+
+    }*/
+
+
+    //hvis den gitte kombinasjonen er riktig så hopper vi til neste activity(menu)
+    //ønsker senere at hvis denne er lik en verdi fra databasen, da blir den gyldig/beveger oss til neste aktivitet
+    public void validate(String number, String password){
+        if((number.length() == 8) && (password.equals("sushi"))){
+            Intent intent = new Intent(LoginActivity.this, MenuActivity.class);
+            startActivity(intent);
+
+        }
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
--- app/src/main/java/com/example/nigirifallsapp/RegisterActivity.java
+++ app/src/main/java/com/example/nigirifallsapp/RegisterActivity.java
@@ -1,13 +1,45 @@
 package com.example.nigirifallsapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.view.View;
+import android.widget.EditText;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    Button registerBtn;
+    EditText name;
+    EditText phoneNr;
+    EditText password;
+    EditText confirm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        registerBtn = (Button)findViewById(R.id.Register);
+        name = (EditText)findViewById(R.id.editText2);
+        phoneNr = (EditText)findViewById(R.id.editText6);
+        password = (EditText)findViewById(R.id.editText3);
+        confirm = (EditText)findViewById(R.id.editText4);
+
+        registerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                validate(name.getText().toString(), phoneNr.getText().toString(), password.getText().toString(), confirm.getText().toString());
+            }
+        });
     }
+
+    //hvis navn, nummer, passord og bekreft passord er fylt ut, så vil registrer knappen få funksjonalitet og vi kan gå videre
+    public void validate(String name, String number, String password, String confirm){
+        if((name.length() > 0) && (number.length() == 8) && (password.length() > 0) && (confirm.equals(password))){
+            Intent intent = new Intent(RegisterActivity.this, MenuActivity.class);
+            startActivity(intent);
+        }
+    }
+
 }
