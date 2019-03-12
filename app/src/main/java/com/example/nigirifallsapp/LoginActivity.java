8da406178361ae9612f5420e2484d935cf518361
Magnus Tidemann <magnuti@stud.ntnu.no>
2019-03-12 20:22:21 +0100

    Now you can login as an ordinary user from the database

--- app/src/main/java/com/example/nigirifallsapp/LoginActivity.java
+++ app/src/main/java/com/example/nigirifallsapp/LoginActivity.java
@@ -7,6 +7,12 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 
 public class LoginActivity extends AppCompatActivity {
@@ -17,6 +23,8 @@ public class LoginActivity extends AppCompatActivity {
     EditText password;
     TextView error_nr; //error message number
     TextView error_pw; //error message password
+    RequestQueue requestQueue;
+    String phoneNumber;
 
 
     @Override
@@ -30,44 +38,29 @@ public class LoginActivity extends AppCompatActivity {
         this.registerBtn = findViewById(R.id.Register1);
         error_nr = (TextView)findViewById(R.id.error_nr);
         error_pw = (TextView)findViewById(R.id.error_pw);
+        this.requestQueue = Volley.newRequestQueue(this);
+
 
 
         loginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                // 1000-for loop???
                 //for (int i = 0; i < 1000; i++){
-                    validate(phoneNr.getText().toString(), password.getText().toString());
+                    phoneNumber = phoneNr.getText().toString().trim();
+                    validate(phoneNumber, password.getText().toString());
                 //}
             }
         });
 
-        registerBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
-                startActivity(intent);
-            }
-        });
     }
 
-    /*
-    //Function for moving to the Menu-activity
-    public void onButtonMenu(View view){
-
-
-    }
-
-
-    //move to registerActivity
     public void onButtonRegister(View view){
+        Intent registerIntent = new Intent(this, RegisterActivity.class);
+        startActivity(registerIntent);
+    }
 
-
-
-    }*/
-
-
-    //hvis den gitte kombinasjonen er riktig så hopper vi til neste activity(menu)
-    //ønsker senere at hvis denne er lik en verdi fra databasen, da blir den gyldig/beveger oss til neste aktivitet
+    // If the username and password matches a user in the database, the next acitivity is MenuActivity
     public void validate(String number, String password) {
         error_nr.setText("");
         error_pw.setText("");
@@ -76,38 +69,39 @@ public class LoginActivity extends AppCompatActivity {
             Intent adminIntent = new Intent(this, AdminActivity.class);
             startActivity(adminIntent);
         }
-        if ((number.length() == 8) && (password.equals("sushi"))) {
-            Intent intent = new Intent(this, MenuActivity.class);
-            startActivity(intent);
 
-        }
-        //error messages if one or both of the input are not valid
-        else {
-            if (number.length() != 8) {
-                error_nr.setText("Invalid number! A number must consist of 8 digits");
-
-            }
-
-            if (!password.equals("sushi")){
-                error_pw.setText("Invalid password");
-
-            }
-
-        }
+        String url = "https://org.ntnu.no/nigiriapp/login.php/?userID=";
+        url += number;
+        url += "&password=";
+        url += password;
+        sendRequest(url);
     }
 
+    // Function for sending a HTTP request to the PHP-script
+    private void sendRequest(String url) {
+        // The requests are sent in cleartext over HTTP. Use HTTPS when sending passwords.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                onActualResponse(response); // The extra function is needed because of the scope of the @Override function
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
 
-    /*public void cleanUpErrorMessage(){
-        if(){
+            }
+        });
 
-        }}*/
+        requestQueue.add(stringRequest);
+    }
 
-
-
-
-
-
-
-
+    private void onActualResponse(String response){
+        if(response.trim().equals(this.phoneNumber)){
+            Intent menuIntent = new Intent(this, MenuActivity.class);
+            startActivity(menuIntent);
+        } else{
+            error_pw.setText(response);
+        }
+    }
 
 }
