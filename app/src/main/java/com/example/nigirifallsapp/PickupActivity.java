ce745dc76882b814f0507b02ddf41e2027ca8eda
theodor <theodolh@stud.ntnu.no>
2019-03-13 12:44:52 +0100

    pickup time solved

--- app/src/main/java/com/example/nigirifallsapp/PickupActivity.java
+++ app/src/main/java/com/example/nigirifallsapp/PickupActivity.java
@@ -1,15 +1,20 @@
 package com.example.nigirifallsapp;
 
 import android.content.Intent;
+import android.app.TimePickerDialog;
+import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.TimePicker;
 
-public class PickupActivity extends AppCompatActivity {
+public class PickupActivity extends AppCompatActivity implements TimePickerDialog.OnTimeSetListener {
 
     Button returnBtn;
     Button placeOrderBtn;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,8 +23,23 @@ public class PickupActivity extends AppCompatActivity {
 
         this.returnBtn = findViewById(R.id.return_);
         this.placeOrderBtn = findViewById(R.id.place_order);
+        Button chooseTime = (Button) findViewById(R.id.button);
+        chooseTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DialogFragment timePicker = new TimePickerFragment();
+                timePicker.show(getSupportFragmentManager(), "Pick time");
+            }
+        });
+
     }
 
+
+    public void onTimeSet(TimePicker view, int hourOfDay, int min){
+        TextView textview = (TextView) findViewById(R.id.textView);
+        textview.setText("Hour: " + hourOfDay + "Minute: " + min);
+
+    }
 
     public void onButtonPlaceOrder(View view){
         Intent intent = new Intent(this, ConfirmationActivity.class);
@@ -31,6 +51,7 @@ public class PickupActivity extends AppCompatActivity {
         Intent intent = new Intent(this, CheckoutActivity.class);
         startActivity(intent);
     }
+
 }
 
 
