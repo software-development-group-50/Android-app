da64e77786f153bd230a75e8e5b7f16e26dbacc0
johabru <johabru@stud.ntnu.no>
2019-03-28 13:37:52 +0100

    php script now runs, but something related to layout is crashing

--- app/src/main/java/com/example/nigirifallsapp/OrderHistory.java
+++ app/src/main/java/com/example/nigirifallsapp/OrderHistory.java
@@ -1,8 +1,10 @@
 package com.example.nigirifallsapp;
 
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.NavUtils;
 import android.support.v4.view.GravityCompat;
@@ -12,8 +14,12 @@ import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -22,11 +28,18 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class OrderHistory extends AppCompatActivity {
 
     RequestQueue requestQueue;
     LinearLayout linearLayoutHistory;
-
+    private int chosenDishIndex;
+    //private int chosenDishID;
+    private int defaultColor;
+    SharedPreferences sharedPreferences;
+    String userID;
     private DrawerLayout drawerLayout;
 
 
@@ -37,6 +50,8 @@ public class OrderHistory extends AppCompatActivity {
         this.requestQueue = Volley.newRequestQueue(this);
         this.linearLayoutHistory = findViewById(R.id.linearLayoutHistory);
         this.drawerLayout = findViewById(R.id.drawer_layout);
+        this.sharedPreferences = getSharedPreferences("login", MODE_PRIVATE);
+        this.userID = sharedPreferences.getString("phonenumber", "error");
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -59,8 +74,7 @@ public class OrderHistory extends AppCompatActivity {
                         return true;
                     }
                 });
-
-        this.sendRequestGetUserOrders("http://folk.ntnu.no/magnuti/getuserorders.php");
+        this.sendRequestGetUserOrders("http://org.ntnu.no/nigiriapp/getuserorders.php/?userID=" + userID);
     }
 
     @Override
@@ -99,9 +113,66 @@ public class OrderHistory extends AppCompatActivity {
     }
 
     private void onActualResponseGetUserOrders(String response){
-
+        List<OrderInAdmin> orderList = new ArrayList<>();
+        String[] arrayWithStringOrders = response.split(";");
+        for (String elementsInStringArray : arrayWithStringOrders) {
+            OrderInAdmin orderInAdmin = new OrderInAdmin(elementsInStringArray);
+            orderList.add(orderInAdmin);
+        }
+        addMenuToView(orderList);
     }
 
+    private void addMenuToView(List<OrderInAdmin> orderInAdminList) {
+        // Reversed for-loop, since we want newest order on top
+        for (int i = orderInAdminList.size() - 1; i >= 0; i--) {
+            LayoutInflater outerLayoutInflater = (LayoutInflater) getApplicationContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            View orderInAdminView = outerLayoutInflater.inflate(R.layout.order_in_admin_layout, null);
+            final int orderid = Integer.valueOf(orderInAdminList.get(i).getOrderId().trim());
+
+            final TextView textOrders = orderInAdminView.findViewById(R.id.textOrderID);
+            final TextView textPickUpTime = orderInAdminView.findViewById(R.id.textPickUpTime);
+            final TextView textOrderStatus = orderInAdminView.findViewById(R.id.textOrderStatus);
+
+            textOrders.setText(orderInAdminList.get(i).getOrderId());
+            String timeString = orderInAdminList.get(i).getPickUpTime();
+            textPickUpTime.setText(timeString.substring(0, timeString.length() - 3));
+            textOrderStatus.setText(orderInAdminList.get(i).getStatus());
+
+            ViewGroup outerInsertPoint = (ViewGroup) findViewById(R.id.linearLayoutAdmin);
+            outerInsertPoint.addView(orderInAdminView, -1, new ViewGroup.LayoutParams(ViewGroup.LayoutParams.FILL_PARENT, ViewGroup.LayoutParams.FILL_PARENT));
+            //Index -1 -> older orders are placed to the bottom
+
+            for (int k = 0; k < orderInAdminList.get(i).getDishList().size(); k += 2) {
+                LayoutInflater innerLayoutInflater = (LayoutInflater) getApplicationContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                View dishInOrderInAdminView = innerLayoutInflater.inflate(R.layout.dish_in_order_in_admin_layout, null);
+
+                TextView textDishName = dishInOrderInAdminView.findViewById(R.id.textDishName);
+                TextView textQuantity = dishInOrderInAdminView.findViewById(R.id.textQuantity);
+
+                textDishName.setText(orderInAdminList.get(i).getDishList().get(k));
+                textQuantity.setText(orderInAdminList.get(i).getDishList().get(k + 1));
+
+                ViewGroup innerInsertPoint = (ViewGroup) findViewById(R.id.linearLayoutOrder);
+                innerInsertPoint.addView(dishInOrderInAdminView, -1, new ViewGroup.LayoutParams(ViewGroup.LayoutParams.FILL_PARENT, ViewGroup.LayoutParams.FILL_PARENT));
+            }
+
+            final LinearLayout linearLayoutOrder = findViewById(R.id.linearLayoutOrder);
+            this.defaultColor = linearLayoutOrder.getSolidColor(); // I don´t know the default color in Android Studio, so it is fetched here.
+            linearLayoutOrder.setId(orderInAdminList.size() - (i + 1)); // This line is required so that not all orders are placed into the same linearLayoutOrder.
+            //The setID is indexed as if the IDs are counting from top to bottom (0, 1, 2...)
+            final int linearLayoutOrderIndex = linearLayoutOrder.getId();
+
+            this.linearLayoutHistory.getChildAt(linearLayoutOrderIndex).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    linearLayoutHistory.getChildAt(chosenDishIndex).setBackgroundColor(defaultColor);
+                    linearLayoutHistory.getChildAt(linearLayoutOrderIndex).setBackgroundColor(Color.GRAY);
+                    chosenDishIndex = linearLayoutOrderIndex;
+                    //chosenDishID = orderid;
+                }
+            });
+        }
+    }
 
     private void logOutAlert() {
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
